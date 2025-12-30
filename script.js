aae6c42fbe1e68d8de84c57ae115848049e7ed4d
AlexLieb83 <alexliebhaber1992@gmail.com>
2025-12-30 09:19:54 -0600

    creating a grid using js and flexbox

---
+++ script.js
@@ -0,0 +1,21 @@
+const containerDiv = document.getElementById("container");
+
+const SQUARE_COUNT = 16;
+const SQUARE_SIZE = 32;
+//how many squares in one row -- based on square_count
+const side = Math.sqrt(SQUARE_COUNT);
+
+//container must be this wide to fit the proper square amount per row
+//4 squares per row
+//each square 32 size
+//so width must be 4 * 32 = 128px
+containerDiv.style.width = `${side * SQUARE_SIZE}px`;
+
+for (let i = 0; i < SQUARE_COUNT; i++) {
+  const squareDiv = document.createElement("div");
+  squareDiv.style.width = `${SQUARE_SIZE}px`;
+  squareDiv.style.height = `${SQUARE_SIZE}px`;
+  squareDiv.style.border = "2px solid black";
+
+  containerDiv.appendChild(squareDiv);
+}
